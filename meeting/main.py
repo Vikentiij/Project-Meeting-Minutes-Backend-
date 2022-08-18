5a27c8bdac9c502e1b7eb2ebd2e5158cd9ef26a2
Viktoriia Protopopova <vikentij.dean@gmail.com>
2022-08-18 17:44:30 +1200

    CRD done

--- meeting/main.py
+++ meeting/main.py
@@ -1,5 +1,5 @@
 from turtle import title
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -24,12 +24,30 @@ def create(request: schemas.Meeting, db: Session = Depends(get_db)):
     db.refresh(new_meeting)
     return new_meeting
 
+@app.delete("/meeting/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    db.query(models.Meeting).filter(models.Meeting.id == id).delete(synchronize_session=False)
+    db.commit()
+    return "done"
+
+@app.put("/meeting/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Meeting, db: Session = Depends(get_db)):
+    db.query(models.Meeting).filter(models.Meeting.id == id).update(request, synchronize_session=False) #update({"title":"updated title"}) #update(request)
+    db.commit()
+    return "updated"
+
 @app.get("/meeting")
 def all(db: Session = Depends(get_db)):
     meetings = db.query(models.Meeting).all()
     return meetings
 
-@app.get("/meeting/{id}")
-def show(id, db: Session = Depends(get_db)):
+@app.get("/meeting/{id}", status_code=200)
+def show(id, response:Response, db: Session = Depends(get_db)):
     meeting = db.query(models.Meeting).filter(models.Meeting.id==id).first()
+    if not meeting:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                             detail= f"Meeting with id {id} is not avaliable")
+
+       #response.status_code = status.HTTP_404_NOT_FOUND
+        #return{"detail":f"Meeting with id {id} is not avaliable"}
     return meeting
